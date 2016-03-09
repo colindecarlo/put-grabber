24b7ff0c4c3df6d39a649f29a879aa1d440597a3
Colin DeCarlo <colin@thedecarlos.ca>
2016-03-09 00:45:39 -0500

    Remove the welcome template and add a home controller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
 
 Route::resource('transfer', 'TransferController', ['only' => 'store']);
 Route::resource('settings', 'SettingsController', ['only' => 'index']);
